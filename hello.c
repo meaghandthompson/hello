58de30c047ebfa7f29c5f6653f0f3f4782dbb1ab
Meaghan Thompson <meaghster@gmail.com>
2022-06-09 20:06:52 -0700

    now takes name as input

--- hello.c
+++ hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        printf("hello, %s\n", argv[1]);
-    } else {
-        printf("please provide a name");
-    }
+    printf("What is your name?");
+    char name[25];
+    scanf("%s", name);
+    printf("hello, %s\n", name);
     return 7;
 }
